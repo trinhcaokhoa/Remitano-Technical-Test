5fc633f1fe3cf9a0e5551ece2ef58f5cd996e772
trinhcaokhoa <caokhoatrinh@gmail.com>
2026-04-27 16:19:59 -0700

    FIX env error

--- src/server/redis.ts
+++ src/server/redis.ts
@@ -1,9 +1,5 @@
 import { Redis } from "ioredis";
 
-if (!process.env.REDIS_URL) {
-  throw new Error("REDIS_URL is not defined");
-}
-
-export const redis = new Redis(process.env.REDIS_URL, {
+export const redis = new Redis(process.env.REDIS_URL!, {
   maxRetriesPerRequest: null,
 });
