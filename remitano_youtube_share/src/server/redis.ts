639aa7ccd69ba645044e930255c96f432f81231d
trinhcaokhoa <caokhoatrinh@gmail.com>
2026-04-27 10:24:13 -0700

    FIX deploy error

--- remitano_youtube_share/src/server/redis.ts
+++ remitano_youtube_share/src/server/redis.ts
@@ -1,8 +1,5 @@
 import { Redis } from "ioredis";
 
-export const redis = new Redis({
-  host: "127.0.0.1",
-  port: 6379,
-
+export const redis = new Redis(process.env.REDIS_URL ?? "redis://127.0.0.1:6379", {
   maxRetriesPerRequest: null,
 });
